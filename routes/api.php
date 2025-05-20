cd2ea04529c9e8ea05e9aeefb4fc27d8f5613e69
Jimmy <toluadejimi@gmail.com>
2025-05-20 17:22:34 +0100

    pushhhhhh

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::any('submit',  [HomeController::class,'submit']);
+Route::any('validate',  [HomeController::class,'validate_code']);
 
 
 
