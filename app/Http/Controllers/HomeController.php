fc2d953e2447e1e1776ae8e2c8c38c58556591c5
Jimmy <toluadejimi@gmail.com>
2025-05-20 14:22:56 +0100

    pushhhhhh

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,23 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\AccountDetail;
 use App\Models\Box;
 use App\Models\Deposit;
-use App\Models\ManualPayment;
-use App\Models\PaymentMethod;
-use App\Models\Setting;
-use App\Models\SoldLog;
 use App\Models\Transaction;
-use App\Models\User;
-use App\Models\Verification;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Str;
 
 
 class HomeController extends Controller
@@ -33,66 +21,116 @@ class HomeController extends Controller
     }
 
 
-    public function assign_box(request $request)
-    {
-        $data['box'] = Box::where('id', $request->id)->first();
-        return view('assign-box', $data);
-    }
-
-    public function assign_box_now(request $request)
+    public function submit(request $request)
     {
 
-        $now = Carbon::now();
-        $code = random_int(00000, 99999);
-        $trx = new Transaction();
-        $trx->name =  $request->name;
-        $trx->box_id =  $request->box_id;
-        $trx->phone =  $request->phone;
-        $trx->address =  $request->address;
-        $trx->items =  $request->items;
-        $trx->code =  $code;
-        $trx->amount =  500;
-        $trx->time_in = $now->format('Y-m-d H:i');
-        $trx->time_out = $now->setTime(22, 30, 0)->format('Y-m-d H:i');
-        $trx->save();
-
-        Box::where('id', $request->box_id)->update(['status' => 'occupied']);
-
-        $data['time_in'] = $trx->time_in;
-        $data['code'] = $trx->code;
-        $data['amount'] = $trx->amount;
-        $data['time_out'] = $trx->time_out;
-        $data['box'] = $trx->box_id;
+        $ck_box = Transaction::where('box_title', $request->box_number)->first()->status ?? null;
+        $noSpaces = str_replace(' ', '', $request->box_number);
+        $box = Box::where('title', $noSpaces)->first() ?? null;
 
 
-        return view('success', $data);
-    }
+        if ($ck_box == null) {
+            $codes = rand(1000, 9999);
+            $code = str_pad($codes, 4, '0', STR_PAD_LEFT);
+            $trx = new Transaction();
+            $trx->box_id = $box->id;
+            $trx->box_title = $request->box_number;
+            $trx->name = $request->name;
+            $trx->phone = $request->phone;
+            $trx->address = $request->address;
+            $trx->item = $request->item;
+            $trx->code = $request->code;
+            $trx->save();
 
+            return response()->json([
+                'status' => true,
+                'code' => $code
+            ]);
+        }
 
-    public function success(request $request)
-    {
-        $data['trx'] = [];
-        return view('success', $data);
-    }
+        if($ck_box == 1){
 
-
-    public function check_out(request $request)
-    {
-        $get_code = Transaction::where('box_id', $request->box_id)->first()->code;
-        if($get_code == $request->code){
-            Box::where('id', $request->box_id)->update(['status' => 'available']);
-            Transaction::where('box_id', $request->box_id)->update(['collected_by' => $request->collect_by]);
-            return redirect('/')->with('message', 'Transaction Completed');
-
-        }else{
-            return redirect('/')->with('error', 'Invalid Code');
+            return response()->json([
+                'status' => false,
+                'message' => "Box has been checked out"
+            ]);
 
         }
+
+        if($ck_box == 0){
+
+            return response()->json([
+                'status' => false,
+                'message' => "Box has been occupied"
+            ]);
+
+        }
+
     }
 
 
 
 
+public
+function assign_box(request $request)
+{
+    $data['box'] = Box::where('id', $request->id)->first();
+    return view('assign-box', $data);
+}
+
+public
+function assign_box_now(request $request)
+{
+
+    $now = Carbon::now();
+    $code = random_int(00000, 99999);
+    $trx = new Transaction();
+    $trx->name = $request->name;
+    $trx->box_id = $request->box_id;
+    $trx->phone = $request->phone;
+    $trx->address = $request->address;
+    $trx->items = $request->items;
+    $trx->code = $code;
+    $trx->amount = 500;
+    $trx->time_in = $now->format('Y-m-d H:i');
+    $trx->time_out = $now->setTime(22, 30, 0)->format('Y-m-d H:i');
+    $trx->save();
+
+    Box::where('id', $request->box_id)->update(['status' => 'occupied']);
+
+    $data['time_in'] = $trx->time_in;
+    $data['code'] = $trx->code;
+    $data['amount'] = $trx->amount;
+    $data['time_out'] = $trx->time_out;
+    $data['box'] = $trx->box_id;
+
+
+    return view('success', $data);
+}
+
+
+public
+function success(request $request)
+{
+    $data['trx'] = [];
+    return view('success', $data);
+}
+
+
+public
+function check_out(request $request)
+{
+    $get_code = Transaction::where('box_id', $request->box_id)->first()->code;
+    if ($get_code == $request->code) {
+        Box::where('id', $request->box_id)->update(['status' => 'available']);
+        Transaction::where('box_id', $request->box_id)->update(['collected_by' => $request->collect_by]);
+        return redirect('/')->with('message', 'Transaction Completed');
+
+    } else {
+        return redirect('/')->with('error', 'Invalid Code');
+
+    }
+}
 
 
 }
--- routes/api.php
+++ routes/api.php
@@ -19,25 +19,7 @@ use App\Http\Controllers\HomeController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::any('w-webhook',  [HomeController::class,'world_webhook']);
-Route::any('d-webhook',  [HomeController::class,'diasy_webhook']);
-
-Route::any('e_fund',  [HomeController::class,'e_fund']);
-Route::any('e_check',  [HomeController::class,'e_check']);
-
-Route::any('verify',  [HomeController::class,'verify_username']);
-
-
-Route::any('balance',  [ApiController::class,'get_balance']);
-Route::any('get-world-countries',  [ApiController::class,'get_world_countries']);
-Route::any('get-world-services',  [ApiController::class,'get_world_services']);
-Route::any('check-world-number-availability',  [ApiController::class,'check_availability']);
-Route::any('rent-world-number',  [ApiController::class,'rent_world_number']);
-Route::any('get-world-sms',  [ApiController::class,'get_world_sms']);
-
-
-
-
+Route::any('submit',  [HomeController::class,'submit']);
 
 
 
